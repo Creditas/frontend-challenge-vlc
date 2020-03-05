0a3669bacf05b23aba8f73768d2c44737eca8893
Christian Wilson <christian.wilson.etiaba@gmail.com>
2020-03-05 09:45:56 +0100

    feauture: challenge frontend vlc

--- src/index.js
+++ src/index.js
@@ -10,6 +10,9 @@ export const getFormValues = formElement =>
       value: element.value
     }))
 
+export const confirm = formElement => formElement.checkValidity(true)
+export const Alert = formElement => formElement.checkValidity()
+
 export const toStringFormValues = values => {
   const match = matchString => value => value.field === matchString
   const IOF = 6.38 / 100
@@ -20,11 +23,11 @@ export const toStringFormValues = values => {
   return `OUTPUT\n${values
     .map(value => `${value.field} --> ${value.value}`)
     .join('\n')}`.concat(
-      `\nTotal ${(IOF + INTEREST_RATE + NUMBER_OF_INSTALLMENTS + 1) * VEHICLE_LOAN_AMOUNT}`
-    )
+    `\nTotal ${(IOF + INTEREST_RATE + NUMBER_OF_INSTALLMENTS + 1) * VEHICLE_LOAN_AMOUNT}`
+  )
 }
 
-export function Send(values) {
+export function Send (values) {
   return new Promise((resolve, reject) => {
     try {
       resolve(toStringFormValues(values))
@@ -34,7 +37,7 @@ export function Send(values) {
   })
 }
 
-export function Submit(formElement) {
+export function Submit (formElement) {
   formElement.addEventListener('submit', function (event) {
     event.preventDefault()
     if (checkFormValidity(formElement)) {
@@ -45,40 +48,54 @@ export function Submit(formElement) {
   })
 }
 
-export function Help(element) {
+export function Help (element) {
   element.addEventListener('click', function (event) {
-    alert('Display here the help text')
+    alert('Can you help me?')
+    setTimeout(function () {
+      alert('Yes, sure')
+    }, 10000)
   })
 }
 
-export function handleChangeRangeVehicleUnderWarranty(
+export function handleChangeRangeVehicleUnderWarranty (
   warrantyRangeElement,
   vehicleWarrantyElement
 ) {
-  const MIN_VALUE = 12000.0
+  const MIN_VALUE = 5000.0
+  const MAX_VALUE = 3000000.00
+
   warrantyRangeElement.addEventListener('change', function (event) {
-    vehicleWarrantyElement.value =
-      (Number(MIN_VALUE) * Number(event.target.value)) / 100 + Number(MIN_VALUE)
+    vehicleWarrantyElement.value = Math.round(Number(event.target.value) * (MAX_VALUE - MIN_VALUE + 1)) + MIN_VALUE
   })
+
+  if (document.getElementById('collateral').selected === 'home') {
+    document.getElementById('min-collateral').innerHTML = 80000.0
+    document.getElementById('max-collateral').innerHTML = 4500000.0
+  }
 }
 
-export function handleChangeVehicleLoanAmount(
+export function handleChangeVehicleLoanAmount (
   loanAmountRangeElement,
   loanAmountElement
 ) {
-  const MIN_VALUE = 30000.0
+  const MIN_VALUE = 3000.0
+  const MAX_VALUE = 100000.0
+
   loanAmountRangeElement.addEventListener('change', function (event) {
-    loanAmountElement.value =
-      (Number(MIN_VALUE) * Number(event.target.value)) / 100 + Number(MIN_VALUE)
+    loanAmountElement.value = Math.round(Number(event.target.value) * (MAX_VALUE - MIN_VALUE + 1)) + MIN_VALUE
   })
+  if (document.getElementById('collateral').selected === 'home') {
+    document.getElementById('min-loan').innerHTML = 50000.0
+    document.getElementById('max-loan').innerHTML = 100000000.0
+  }
 }
 
 export default class CreditasChallenge {
-  static initialize() {
+  static initialize () {
     this.registerEvents()
   }
 
-  static registerEvents() {
+  static registerEvents () {
     Submit(document.querySelector('.form'))
     Help(document.getElementById('help'))
 
